49abc804cce116b1aaad09fb18a8dade35aad408
tinman00 <117823440+tinman00@users.noreply.github.com>
2025-12-18 18:47:56 +0800

    stronger load scene test

--- TestScene.cpp
+++ TestScene.cpp
@@ -2,19 +2,21 @@
 #include "TestScene2.h"
 #include "Button.h"
 #include "SceneManager.h"
+#include "Text.h"
+#include "SFML/System.hpp"
 #include <iostream>
 
 TestScene::TestScene()
 {
 	std::cout << "Loading test scene 1" << std::endl;
 	objects.push_back(new Button(
-		{ 800, 350 },
-		{ 400, 200 },
+		{ 300, 300 },
+		{ 240, 200 },
 		2,
 		[]() {
-			std::cout << "Clicking button 2" << std::endl;
+			std::cout << "Clicking button 1" << std::endl;
 			SceneManager::LoadScene(new TestScene2());
 		}
 	));
-	objects.push_back(new Text({ 400,250 }, "Hello world", sf::Color(0, 0, 0), "Arial.ttf", 24));
+	objects.push_back(new Text({ 300,300 }, L"ÈË£¬¹ýÀ´", sf::Color(255, 127, 63), "Deng.ttf", 32));
 }
